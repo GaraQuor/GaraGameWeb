e22b480b715ac712c21070edf51451272d22fe56
Shadowsun <wendyfreedom31@hotmail.com>
2016-06-02 15:16:01 +0200

    gamepad phaser

---
+++ gamepad.js
@@ -0,0 +1,47 @@
+var game = new Phaser.Game(600, 400, Phaser.CANVAS, 'game', {preload: fnPreload, create: fnCreate, update: fnUpdate});
+
+function fnPreload() {
+  // On charge les assets du jeu ici
+  console.log("Game is loading");
+
+  var path = 'img/';
+
+  game.load.image('buttonA', path + 'lineLight34.png');
+  game.load.image('buttonB', path + 'lineLight35.png');
+  game.load.image('buttonS', path + 'lineLight33.png');
+  game.load.image('gamepad', path + 'lineLight02.png');
+  // game.load.image('left', path + 'yellow_sliderLeft.png');
+  // game.load.image('right', path + 'red_sliderRight.png');
+  // game.load.image('up', path + 'blue_sliderUp.png');
+  // game.load.image('down', path + 'grey_sliderDown.png');
+}
+
+var gamepad, buttonA, buttonB, buttonS, score;
+
+function fnCreate() {
+  game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+  gamepad = game.add.button(100,game.height/2,'gamepad',butClick);
+  gamepad.anchor.setTo(0.5, 0.5);
+  buttonA = game.add.button(game.width -80,game.height/2 -40,'buttonA',butClick);
+  buttonA.anchor.setTo(0.5, 0.5);
+  buttonB = game.add.button(game.width-160,game.height/2 +40,'buttonB',butClick);
+  buttonB.anchor.setTo(0.5, 0.5);
+  buttonS = game.add.button(game.width/2,game.height/2,'buttonS',butClick);
+  buttonS.anchor.setTo(0.5, 0.5);
+
+  score = game.add.text(game.width/2, 50, 'Score : 150', {font: '64px Orbitron', fill: '#fff'} );
+  score.anchor.setTo(.5,.5);
+
+
+
+
+}
+
+function fnUpdate() {
+
+}
+
+function butClick() {
+  console.log("click");
+}
